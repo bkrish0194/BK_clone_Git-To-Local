b91a69c8877a2323ad1918bb7de05ba0712ee8d2
bkrish0194 <balakrishnak111@gmail.com>
2022-07-24 01:00:27 +0530

    Pushing second file

---
+++ Day2.py
@@ -0,0 +1,14 @@
+
+
+
+stringName = "Hello Earth"
+print(stringName.upper())
+
+x1 = 100
+print('x1 = ', x1)
+print(  "Binary Value of x1 = ", bin(x1) )
+print(  "Hex Value of x1 = ", hex(x1) )
+
+
+
+
